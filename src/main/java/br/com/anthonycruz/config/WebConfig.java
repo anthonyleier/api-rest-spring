993e37b8883ddef989d17abffbd3c2a24f0fdeca
Anthony Cruz <anthonyleierlw@gmail.com>
2024-05-10 11:22:35 -0300

    Content Negotiation com Header

--- src/main/java/br/com/anthonycruz/config/WebConfig.java
+++ src/main/java/br/com/anthonycruz/config/WebConfig.java
@@ -11,9 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 		configurer
-		.favorParameter(true)
-		.parameterName("mediaType")
-		.ignoreAcceptHeader(true)
+		.favorParameter(false)
+		.ignoreAcceptHeader(false)
 		.useRegisteredExtensionsOnly(false)
 		.defaultContentType(MediaType.APPLICATION_JSON)
 		.mediaType("json", MediaType.APPLICATION_JSON)
