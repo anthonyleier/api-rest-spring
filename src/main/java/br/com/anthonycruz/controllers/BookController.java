d2eecb591c1268b33d03c1fda60f10cb7bfe58b2
Anthony Cruz <anthonyleierlw@gmail.com>
2024-05-14 16:39:09 -0300

    Definição dos endpoints restantes de Book

--- src/main/java/br/com/anthonycruz/controllers/BookController.java
+++ src/main/java/br/com/anthonycruz/controllers/BookController.java
@@ -1,7 +1,14 @@
 package br.com.anthonycruz.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +19,33 @@ import br.com.anthonycruz.services.BookService;
 @RestController
 @RequestMapping("/books")
 public class BookController {
-	
+
 	@Autowired
 	private BookService service;
-	
+
+	@GetMapping
+	public List<BookDTO> findAll() {
+		return service.findAll();
+	}
+
+	@GetMapping(value = "/{id}")
+	public BookDTO findById(@PathVariable(value = "id") Long id) {
+		return service.findById(id);
+	}
+
 	@PostMapping
 	public BookDTO create(@RequestBody BookDTO bookDTO) {
 		return service.create(bookDTO);
+	}
+
+	@PutMapping
+	public BookDTO update(@RequestBody BookDTO bookDTO) {
+		return service.update(bookDTO);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/br/com/anthonycruz/services/BookService.java
+++ src/main/java/br/com/anthonycruz/services/BookService.java
@@ -1,9 +1,12 @@
 package br.com.anthonycruz.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.anthonycruz.data.dto.v1.BookDTO;
+import br.com.anthonycruz.exceptions.ResourceNotFoundException;
 import br.com.anthonycruz.mapper.DTOMapper;
 import br.com.anthonycruz.models.Book;
 import br.com.anthonycruz.repositories.BookRepository;
@@ -13,9 +16,38 @@ public class BookService {
 	@Autowired
 	BookRepository repository;
 
+	public List<BookDTO> findAll() {
+		var books = repository.findAll();
+		return DTOMapper.parseListObjects(books, BookDTO.class);
+	}
+
+	public BookDTO findById(Long id) {
+		Book book = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		return DTOMapper.parseObject(book, BookDTO.class);
+	}
+
 	public BookDTO create(BookDTO bookDTO) {
 		Book book = DTOMapper.parseObject(bookDTO, Book.class);
 		var savedBook = repository.save(book);
 		return DTOMapper.parseObject(savedBook, BookDTO.class);
+	}
+
+	public BookDTO update(BookDTO bookDTO) {
+		Book book = DTOMapper.parseObject(bookDTO, Book.class);
+
+		book.setTitle(bookDTO.getTitle());
+		book.setAuthor(bookDTO.getAuthor());
+		book.setPrice(bookDTO.getPrice());
+		book.setLaunchDate(bookDTO.getLaunchDate());
+
+		var savedBook = repository.save(book);
+		return DTOMapper.parseObject(savedBook, BookDTO.class);
+	}
+
+	public void delete(Long id) {
+		var book = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		repository.delete(book);
 	}
 }
