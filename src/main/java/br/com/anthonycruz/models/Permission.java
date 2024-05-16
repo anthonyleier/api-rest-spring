5285c0c93118ff29cc8329aa274f1742e63dc3b7
Anthony Cruz <anthonyleierlw@gmail.com>
2024-05-16 16:36:40 -0300

    Modelagem das entidades User e Permission

---
+++ src/main/java/br/com/anthonycruz/models/Permission.java
@@ -0,0 +1,64 @@
+package br.com.anthonycruz.models;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "permission")
+public class Permission implements GrantedAuthority, Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column
+	private String description;
+
+	@Override
+	public String getAuthority() {
+		return this.description;
+	}
+
+	public Permission() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		Permission other = (Permission) obj;
+		return Objects.equals(description, other.description) && Objects.equals(id, other.id);
+	}
+
+}
---
+++ src/main/java/br/com/anthonycruz/models/User.java
@@ -0,0 +1,186 @@
+package br.com.anthonycruz.models;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User implements UserDetails, Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "user_name", unique = true)
+	private String userName;
+
+	@Column(name = "full_name")
+	private String fullName;
+
+	@Column
+	private String password;
+
+	@Column(name = "account_non_expired")
+	private Boolean accountNonExpired;
+
+	@Column(name = "account_non_locked")
+	private Boolean accountNonLocked;
+
+	@Column(name = "credentials_non_expired")
+	private Boolean credentialsNonExpired;
+
+	@Column
+	private Boolean enabled;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "user_permission",
+			joinColumns = { @JoinColumn(name = "id_user") },
+			inverseJoinColumns = { @JoinColumn(name = "id_permission") }
+			)
+	private List<Permission> permissions;
+	
+	public User() {}
+	
+	public List<String> getRoles() {
+		List<String> roles = new ArrayList<>();
+		for (Permission permission : permissions) {
+			roles.add(permission.getDescription());
+		}
+		return roles;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.permissions;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.userName;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return this.accountNonExpired;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return this.accountNonLocked;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return this.credentialsNonExpired;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return this.enabled;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public Boolean getAccountNonExpired() {
+		return accountNonExpired;
+	}
+
+	public void setAccountNonExpired(Boolean accountNonExpired) {
+		this.accountNonExpired = accountNonExpired;
+	}
+
+	public Boolean getAccountNonLocked() {
+		return accountNonLocked;
+	}
+
+	public void setAccountNonLocked(Boolean accountNonLocked) {
+		this.accountNonLocked = accountNonLocked;
+	}
+
+	public Boolean getCredentialsNonExpired() {
+		return credentialsNonExpired;
+	}
+
+	public void setCredentialsNonExpired(Boolean credentialsNonExpired) {
+		this.credentialsNonExpired = credentialsNonExpired;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public List<Permission> getPermissions() {
+		return permissions;
+	}
+
+	public void setPermissions(List<Permission> permissions) {
+		this.permissions = permissions;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(accountNonExpired, accountNonLocked, credentialsNonExpired, enabled, fullName, id, password, permissions, userName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		User other = (User) obj;
+		return Objects.equals(accountNonExpired, other.accountNonExpired) && Objects.equals(accountNonLocked, other.accountNonLocked) && Objects.equals(credentialsNonExpired, other.credentialsNonExpired) && Objects.equals(enabled, other.enabled) && Objects.equals(fullName, other.fullName) && Objects.equals(id, other.id) && Objects.equals(password, other.password) && Objects.equals(permissions, other.permissions) && Objects.equals(userName, other.userName);
+	}
+}
