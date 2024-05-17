7764aa0faa14a7de8b53d9909453c089cb9c0728
Anthony Cruz <anthonyleierlw@gmail.com>
2024-05-17 11:26:08 -0300

    Resolvendo depreciações

--- src/main/java/br/com/anthonycruz/Startup.java
+++ src/main/java/br/com/anthonycruz/Startup.java
@@ -8,19 +8,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
 
 @SpringBootApplication
 public class Startup {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
+		Pbkdf2PasswordEncoder encoder = new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
 		
         Map<String, PasswordEncoder> encoders = new HashMap<>();
-        encoders.put("pbkdf2", new Pbkdf2PasswordEncoder());
+        encoders.put("pbkdf2", encoder);
         DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
-        passwordEncoder.setDefaultPasswordEncoderForMatches(new Pbkdf2PasswordEncoder());
+        passwordEncoder.setDefaultPasswordEncoderForMatches(encoder);
         
-        String result = passwordEncoder.encode("admin234");
-        System.out.println("My hash " + result);
+        String result1 = passwordEncoder.encode("admin123");
+        String result2 = passwordEncoder.encode("admin456");
+        System.out.println("My hash 1 " + result1);
+        System.out.println("My hash 2 " + result2);
 	}
 }
--- src/main/java/br/com/anthonycruz/config/SecurityConfig.java
+++ src/main/java/br/com/anthonycruz/config/SecurityConfig.java
@@ -7,57 +7,65 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
+import org.springframework.security.web.SecurityFilterChain;
 
 import br.com.anthonycruz.security.jwt.JwtConfigurer;
 import br.com.anthonycruz.security.jwt.JwtTokenProvider;
 
+@EnableWebSecurity
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
 	@Autowired
 	private JwtTokenProvider tokenProvider;
 
 	@Bean
-	public PasswordEncoder passwordEncoder() {
+	PasswordEncoder passwordEncoder() {
 		String algorithmType = "pbkdf2";
+		Pbkdf2PasswordEncoder encoder = new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
 		Map<String, PasswordEncoder> encoders = new HashMap<>();
-		encoders.put(algorithmType, new Pbkdf2PasswordEncoder());
+
+		encoders.put(algorithmType, encoder);
 		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder(algorithmType, encoders);
-		passwordEncoder.setDefaultPasswordEncoderForMatches(new Pbkdf2PasswordEncoder());
+		passwordEncoder.setDefaultPasswordEncoderForMatches(encoder);
+
 		return passwordEncoder;
 	}
-	
+
 	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
+	AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
 	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.httpBasic().disable()
-			.csrf().disable()
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				.and()
-					.authorizeRequests()
-					.antMatchers(
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+        		.httpBasic().disable()
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+            		session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                    authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers(
 							"/auth/signin",
-							"/auth/refresh",
-							"/api-docs/**",
-							"/swagger-ui.html**"
-						).permitAll()
-					.antMatchers("/api/**").authenticated()
-					.antMatchers("/users").denyAll()
-				.and()
-					.cors()
-				.and()
-				.apply(new JwtConfigurer(tokenProvider));
-	}
+							"/auth/refresh/**",
+                    		"/swagger-ui/**",
+                    		"/v3/api-docs/**"
+                		).permitAll()
+                        .requestMatchers("/api/**").authenticated()
+                        .requestMatchers("/users").denyAll()
+                )
+                .cors()
+                .and().apply(new JwtConfigurer(tokenProvider))
+                .and().build();
+    }
 }
