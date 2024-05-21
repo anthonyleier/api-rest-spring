4fcfa06d03a24f4d7d4e3d5d3750069dc5a883e8
Anthony Cruz <anthonyleierlw@gmail.com>
2024-05-21 11:50:12 -0300

    Testes para BookController com JSON

---
+++ src/test/java/br/com/anthonycruz/integrationtests/dto/BookDTO.java
@@ -0,0 +1,84 @@
+package br.com.anthonycruz.integrationtests.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class BookDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String title;
+	private String author;
+	private Double price;
+	private Date launchDate;
+	
+	public BookDTO() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Date getLaunchDate() {
+		return launchDate;
+	}
+
+	public void setLaunchDate(Date launchDate) {
+		this.launchDate = launchDate;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(author, id, launchDate, price, title);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BookDTO other = (BookDTO) obj;
+		return Objects.equals(author, other.author) && Objects.equals(id, other.id)
+				&& Objects.equals(launchDate, other.launchDate) && Objects.equals(price, other.price)
+				&& Objects.equals(title, other.title);
+	}
+
+}
