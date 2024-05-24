05dafc5e43defd3a25c87677f93a4257157b2a42
Anthony Cruz <anthonyleierlw@gmail.com>
2024-05-24 16:48:40 -0300

    Endpoint para download de arquivos

--- src/main/java/br/com/anthonycruz/controllers/FileController.java
+++ src/main/java/br/com/anthonycruz/controllers/FileController.java
@@ -6,6 +6,12 @@ import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +26,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Tag(name = "Files", description = "Endpoints for managing files")
 @RestController
@@ -54,5 +61,31 @@ public class FileController {
 	public List<UploadFileResponseDTO> uploadFiles(@RequestParam("files") MultipartFile[] files) {
 		logger.info("Storing files to disc");
 		return Arrays.asList(files).stream().map(file -> uploadFile(file)).collect(Collectors.toList());
+	}
+	
+	//MY_file.txt (.+)
+	@GetMapping(value = "/downloadFile/{fileName:.+}")
+	@Operation(summary = "Download a file", description = "Download a file from the disc", tags = {"Files"}, responses = {
+			@ApiResponse(description = "Success", responseCode = "200", content = @Content(schema = @Schema(implementation = UploadFileResponseDTO.class))),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+			})
+	public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+		logger.info("Reading a file on disc");
+		Resource resource = service.load(fileName);
+		String contentType = "";
+		
+		try {
+			contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+			if (contentType.isBlank()) contentType = "application/octet-stream";
+		} catch (Exception e) {
+			logger.info("Could not determine file type");
+		}
+		
+		return ResponseEntity.ok()
+				.contentType(MediaType.parseMediaType(contentType))
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+				.body(resource);
 	}
 }
--- src/main/java/br/com/anthonycruz/services/FileStorageService.java
+++ src/main/java/br/com/anthonycruz/services/FileStorageService.java
@@ -5,12 +5,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.anthonycruz.config.FileStorageConfig;
 import br.com.anthonycruz.exceptions.FileStorageException;
+import br.com.anthonycruz.exceptions.MyFileNotFoundException;
 
 @Service
 public class FileStorageService {
@@ -27,10 +30,10 @@ public class FileStorageService {
 			throw new FileStorageException("Could not create the directory where uploaded files will be stored", e);
 		}
 	}
-	
+
 	public String store(MultipartFile file) {
 		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-		
+
 		try {
 			if (fileName.contains("..")) throw new FileStorageException("Sorry, filename contains invalid path sequence " + fileName);
 			Path targetLocation = this.fileStorageLocation.resolve(fileName);
@@ -38,6 +41,17 @@ public class FileStorageService {
 			return fileName;
 		} catch (Exception e) {
 			throw new FileStorageException("Could not store file " + fileName, e);
+		}
+	}
+
+	public Resource load(String fileName) {
+		try {
+			Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+			Resource resource = new UrlResource(filePath.toUri());
+			if (resource.exists()) return resource;
+			else throw new MyFileNotFoundException("File not found " + fileName);
+		} catch (Exception e) {
+			throw new MyFileNotFoundException("File not found " + fileName, e);
 		}
 	}
 }
