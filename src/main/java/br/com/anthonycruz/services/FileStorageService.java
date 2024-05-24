a1050d1d4293b7432ca4d9bbfb00b4b2d231a09d
Anthony Cruz <anthonyleierlw@gmail.com>
2024-05-24 15:42:21 -0300

    Service para armazenar o arquivo

---
+++ src/main/java/br/com/anthonycruz/services/FileStorageService.java
@@ -0,0 +1,43 @@
+package br.com.anthonycruz.services;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import br.com.anthonycruz.config.FileStorageConfig;
+import br.com.anthonycruz.exceptions.FileStorageException;
+
+@Service
+public class FileStorageService {
+
+	private final Path fileStorageLocation;
+
+	public FileStorageService(FileStorageConfig fileStorageConfig) {
+		Path path = Paths.get(fileStorageConfig.getUploadDir()).toAbsolutePath().normalize();
+		this.fileStorageLocation = path;
+
+		try {
+			Files.createDirectories(this.fileStorageLocation);
+		} catch (Exception e) {
+			throw new FileStorageException("Could not create the directory where uploaded files will be stored", e);
+		}
+	}
+	
+	public String store(MultipartFile file) {
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+		
+		try {
+			if (fileName.contains("..")) throw new FileStorageException("Sorry, filename contains invalid path sequence " + fileName);
+			Path targetLocation = this.fileStorageLocation.resolve(fileName);
+			Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+			return "";
+		} catch (Exception e) {
+			throw new FileStorageException("Could not store file " + fileName, e);
+		}
+	}
+}
