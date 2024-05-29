b7fd8236269c5bc259e17c6d5cfc941f753a4cd6
Anthony Cruz <anthonyleierlw@gmail.com>
2024-05-29 11:45:01 -0300

    Voltando a apontar os testes para a instância de testes

--- src/test/java/br/com/anthonycruz/config/TestConfig.java
+++ src/test/java/br/com/anthonycruz/config/TestConfig.java
@@ -1,7 +1,7 @@
 package br.com.anthonycruz.config;
 
 public class TestConfig {
-	public static final int SERVER_PORT = 80;
+	public static final int SERVER_PORT = 8888;
 	
 	public static final String HEADER_PARAM_ORIGIN = "Origin";
 	public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
