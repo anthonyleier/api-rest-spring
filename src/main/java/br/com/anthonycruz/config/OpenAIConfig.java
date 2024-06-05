1cb8d29e2e900b75e4615f1ce0bd1ead96e4b68f
Anthony Cruz <anthonyleierlw@gmail.com>
2024-06-05 11:25:07 -0300

    Criação de Bean para RestTemplate

--- src/main/java/br/com/anthonycruz/config/OpenAIConfig.java
+++ src/main/java/br/com/anthonycruz/config/OpenAIConfig.java
@@ -3,7 +3,9 @@ package br.com.anthonycruz.config;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class OpenAIConfig {
@@ -11,4 +13,15 @@ public class OpenAIConfig {
 
 	@Value("${openai.api.key}")
 	String apiKey;
+
+	@Bean
+	RestTemplate template() {
+		logger.info("Initializing RestTemplate");
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getInterceptors().add((request, body, execution) -> {
+			request.getHeaders().add("Authorization", "Bearer " + apiKey);
+			return execution.execute(request, body);
+		});
+		return restTemplate;
+	}
 }
